91cbb5c44dd78e5583ccdc928958e741b2ebe393
bakaYar0 <fpa.aff.air@gmail.com>
2022-01-10 00:09:23 +0300

    Creating basic layout for the app

---
+++ SkillTest/SkillTest/Pages/Education.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkillTest.Pages
+{
+    public class EducationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SkillTest/SkillTest/Pages/Interests.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkillTest.Pages
+{
+    public class InterestsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
